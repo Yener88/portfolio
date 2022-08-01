ca70868258c2bebdf67c2e251be9114529c65d6a
Yener88 <106224876+Yener88@users.noreply.github.com>
2022-08-01 10:46:44 +0200

    01.08.22 1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'portfolio-73a24',
+    appId: '1:596730292487:web:ddfa75ae631846b123bda2',
+    storageBucket: 'portfolio-73a24.appspot.com',
+    apiKey: 'AIzaSyDk6eGXenH5DS3YSr5uOjjPW7xoNa8iMOA',
+    authDomain: 'portfolio-73a24.firebaseapp.com',
+    messagingSenderId: '596730292487',
+  },
   production: true
 };
