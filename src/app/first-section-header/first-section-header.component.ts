8041b02dd9b404ae6a473b5ac6a548074d59db03
Yener88 <106224876+Yener88@users.noreply.github.com>
2023-01-21 11:41:17 +0100

    some changes and fixes

--- src/app/first-section-header/first-section-header.component.ts
+++ src/app/first-section-header/first-section-header.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-section-header',
   templateUrl: './first-section-header.component.html',
+  template: `
+  <div (contextmenu)="onRightClick()">Right click me!</div>
+`,
   styleUrls: ['./first-section-header.component.scss']
 })
 export class FirstSectionHeaderComponent implements OnInit {
 
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+    document.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      console.log('Wer die Konsole nicht ehrt, ist das Programmieren nicht wert!');
+    });
   }
 
   mobileNavbar() {
